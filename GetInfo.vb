62a46dc0da2ee355b8057a2f6dc38742fd9921ba
Nicolas Smith <62912470+NickSmithSoftware@users.noreply.github.com>
2021-10-15 10:16:21 -0500

    added some more while loops and conditions

--- GetInfo.vb
+++ GetInfo.vb
@@ -6,6 +6,7 @@ Dim returnValue As Integer
 Dim timeout As Integer
 Dim waitText As String
 Dim dataText As String
+Dim scrapedText As String
 timeout = 15000
 
 Set ibmCurrentTerminal = ThisFrame.SelectedView.control
@@ -43,9 +44,6 @@ returnValue = ibmCurrentScreen.WaitForText1(timeout, waitText, 23, 2, TextCompar
 ibmCurrentScreen.SendKeys ("ed")
 ibmCurrentScreen.SendControlKey (ControlKeyCode_Transmit)
 
-
-
-
 returnValue = ibmCurrentScreen.WaitForKeyboardEnabled(timeout, 0)
 waitText = "NEXT"
 returnValue = ibmCurrentScreen.WaitForText1(timeout, waitText, 23, 2, TextComparisonOption_MatchCase)
@@ -53,11 +51,32 @@ ibmCurrentScreen.SendKeys ("ee")
 ibmCurrentScreen.SendControlKey (ControlKeyCode_Transmit)
 
 
+ibmCurrentScreen.SendControlKey (ControlKeyCode_Clear)
+ibmCurrentScreen.SendKeys ("ea")
+ibmCurrentScreen.SendControlKey (ControlKeyCode_Transmit)
+
+returnValue = ibmCurrentScreen.WaitForKeyboardEnabled(timeout, 0)
+waitText = "NEXT"
+
+While ibmCurrentScreen.GetText(1, 78, 2) <> "EF"
+    returnValue = ibmCurrentScreen.WaitForText1(timeout, waitText, 23, 2, TextComparisonOption_MatchCase)
+    scrapedText = scrapedText & "," & ibmCurrentScreen.GetTextEx(1, 1, 24, 80, GetTextArea_Block, GetTextWrap_Off, GetTextAttr_Visible, GetTextFlags_CRLF)
+    ibmCurrentScreen.SendControlKey(ControlKeyCode_Transmit)
+    returnValue = ibmCurrentScreen.WaitForKeyboardEnabled(timeout, 0)
+End While
+
+While ibmCurrentScreen.GetText(22, 20, 4) <> data
+    returnValue = ibmCurrentScreen.WaitForText1(timeout, waitText, 23, 1, TextComparisonOption_MatchCase)
+    ibmCurrentScreen.SendControlKey (ControlKeyCode_Transmit)
+    returnValue = ibmCurrentScreen.WaitForKeyboardEnabled(timeout, 0)
+End While
 
 
 returnValue = ibmCurrentScreen.WaitForKeyboardEnabled(timeout, 0)
 waitText = "NEXT"
 returnValue = ibmCurrentScreen.WaitForText1(timeout, waitText, 23, 2, TextComparisonOption_MatchCase)
+
+
 
 
 ibmCurrentScreen.SendKeys ("bn")
