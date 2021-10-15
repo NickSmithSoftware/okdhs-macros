f9587ee06887add59b871f5b7f91a59791972773
NickSmithSoftware <62912470+NickSmithSoftware@users.noreply.github.com>
2021-10-14 19:11:02 -0500

    remove junk, create template info loops

---
+++ GetInfo.vb
@@ -0,0 +1,131 @@
+
+Dim ibmCurrentTerminal As IbmTerminal
+Dim ibmCurrentScreen As IbmScreen
+Dim hiddenTextEntry As String
+Dim returnValue As Integer
+Dim timeout As Integer
+Dim waitText As String
+Dim dataText As String
+timeout = 15000
+
+Set ibmCurrentTerminal = ThisFrame.SelectedView.control
+Set ibmCurrentScreen = ibmCurrentTerminal.screen
+
+ibmCurrentScreen.SendControlKey (ControlKeyCode_Clear)
+
+
+
+returnValue = ibmCurrentScreen.WaitForKeyboardEnabled(timeout, 0)
+ibmCurrentScreen.SendKeys ("ea")
+ibmCurrentScreen.SendControlKey (ControlKeyCode_Transmit)
+
+
+
+returnValue = ibmCurrentScreen.WaitForKeyboardEnabled(timeout, 0)
+waitText = "NEXT"
+returnValue = ibmCurrentScreen.WaitForText1(timeout, waitText, 23, 2, TextComparisonOption_MatchCase)
+ibmCurrentScreen.SendKeys ("eb")
+ibmCurrentScreen.SendControlKey (ControlKeyCode_Transmit)
+
+
+
+returnValue = ibmCurrentScreen.WaitForKeyboardEnabled(timeout, 0)
+waitText = "NEXT"
+returnValue = ibmCurrentScreen.WaitForText1(timeout, waitText, 23, 2, TextComparisonOption_MatchCase)
+ibmCurrentScreen.SendKeys ("ec")
+ibmCurrentScreen.SendControlKey (ControlKeyCode_Transmit)
+
+
+
+returnValue = ibmCurrentScreen.WaitForKeyboardEnabled(timeout, 0)
+waitText = "NEXT"
+returnValue = ibmCurrentScreen.WaitForText1(timeout, waitText, 23, 2, TextComparisonOption_MatchCase)
+ibmCurrentScreen.SendKeys ("ed")
+ibmCurrentScreen.SendControlKey (ControlKeyCode_Transmit)
+
+
+
+
+returnValue = ibmCurrentScreen.WaitForKeyboardEnabled(timeout, 0)
+waitText = "NEXT"
+returnValue = ibmCurrentScreen.WaitForText1(timeout, waitText, 23, 2, TextComparisonOption_MatchCase)
+ibmCurrentScreen.SendKeys ("ee")
+ibmCurrentScreen.SendControlKey (ControlKeyCode_Transmit)
+
+
+
+
+returnValue = ibmCurrentScreen.WaitForKeyboardEnabled(timeout, 0)
+waitText = "NEXT"
+returnValue = ibmCurrentScreen.WaitForText1(timeout, waitText, 23, 2, TextComparisonOption_MatchCase)
+
+
+ibmCurrentScreen.SendKeys ("bn")
+ibmCurrentScreen.SendControlKey (ControlKeyCode_Transmit)
+
+
+
+stopText = "DATA"
+
+returnValue = ibmCurrentScreen.WaitForKeyboardEnabled(timeout, 0)
+waitText = "NEXT"
+returnValue = ibmCurrentScreen.WaitForText1(timeout, waitText, 23, 1, TextComparisonOption_MatchCase)
+
+While ibmCurrentScreen.GetText(22, 20, 4) <> data
+    returnValue = ibmCurrentScreen.WaitForText1(timeout, waitText, 23, 1, TextComparisonOption_MatchCase)
+    ibmCurrentScreen.SendControlKey (ControlKeyCode_Transmit)
+    returnValue = ibmCurrentScreen.WaitForKeyboardEnabled(timeout, 0)
+End While
+
+
+
+
+ibmCurrentScreen.SendControlKey (ControlKeyCode_Clear)
+ibmCurrentScreen.SendKeys ("echl")
+ibmCurrentScreen.SendControlKey (ControlKeyCode_Transmit)
+ibmCurrentScreen.WaitForKeyboardEnabled(timeout, 0)
+
+While ## CONDITION ## <> data
+    returnValue = ## WAIT TEXT LOCATION ##
+    ibmCurrentScreen.SendControlKey (ControlKeyCode_Transmit)
+    returnValue = ibmCurrentScreen.WaitForKeyboardEnabled(timeout, 0)
+End While
+
+
+
+
+ibmCurrentScreen.SendControlKey (ControlKeyCode_Clear)
+ibmCurrentScreen.SendKeys ("cmm")
+ibmCurrentScreen.SendControlKey (ControlKeyCode_Transmit)
+
+While ## CONDITION ## <> STOPCONDITION
+    returnValue = ## WAIT TEXT LOCATION ##
+    ibmCurrentScreen.SendControlKey (ControlKeyCode_Transmit)
+    returnValue = ibmCurrentScreen.WaitForKeyboardEnabled(timeout, 0)
+End While
+
+
+
+
+ibmCurrentScreen.SendControlKey (ControlKeyCode_Clear)
+ibmCurrentScreen.SendKeys ("nl")
+ibmCurrentScreen.SendControlKey (ControlKeyCode_Transmit)
+
+While ## CONDITION ## <> STOPCONDITION
+    returnValue = ## WAIT TEXT LOCATION ##
+    ibmCurrentScreen.SendControlKey (ControlKeyCode_Transmit)
+    returnValue = ibmCurrentScreen.WaitForKeyboardEnabled(timeout, 0)
+End While
+
+
+
+
+ibmCurrentScreen.SendControlKey (ControlKeyCode_Clear)
+ibmCurrentScreen.SendKeys ("py")
+ibmCurrentScreen.SendControlKey (ControlKeyCode_Transmit)
+
+While ## CONDITION ## <> STOPCONDITION
+    returnValue = ## WAIT TEXT LOCATION ##
+    ibmCurrentScreen.SendControlKey (ControlKeyCode_Transmit)
+    returnValue = ibmCurrentScreen.WaitForKeyboardEnabled(timeout, 0)
+End While
